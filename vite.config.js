3ef3c4f352a759f129add8b39626b03fb1855539
supahkarnL <barnney2531@gmail.com>
2022-05-02 04:45:35 +0700

    t

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react", // ignore react stuff
-        "react-dom",
-      ],
-    },
-  },
+  build: {},
   server: {
     // port: 8001,
   },
